407d04c6ffd52a8c7ac5f9a67f30c27a23adb554
Renington Neri <renington.neri@gmail.com>
2019-11-30 16:48:04 -0300

    Adiciona as tags nos seeds

--- app/db/seeds.rb
+++ app/db/seeds.rb
@@ -1,2 +1,11 @@
 puts "Adicionando os usuários iniciais"
 User.create(email: 'test@test.com', password: '123456789')
+
+puts "Adicionando tags iniciais"
+Tag.create([
+    { name: "api" },
+    { name: "json" },
+    { name: "collaboration" },
+    { name: "calendar" },
+    { name: "node" }
+])
