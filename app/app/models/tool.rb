b100b5ada0e01256234b42e7ea846d059fd0f7e3
Renington Neri <renington.neri@gmail.com>
2019-12-01 10:45:43 -0300

    Ajusta método de adicionar tags

--- app/app/models/tool.rb
+++ app/app/models/tool.rb
@@ -7,21 +7,20 @@ class Tool < ApplicationRecord
   end
 
   def self.tag_counts
-    Tag.select('tags.*, count(taggings.tag_id) as count').joins(:taggings).group('taggings.tag_id')
+    Tag
+      .select('tags.*, count(taggings.tag_id) as count')
+      .joins(:taggings)
+      .group('taggings.tag_id')
   end
 
   def tag_list
     tags.map(&:name)
   end
 
-  def tag_list=(names)
-    self.tags = names.map do |n|
+  def add_tags(tags)
+    tags.each do |tag|
       Tag.where(name: n.strip).first_or_create!
     end
-  end
-
-  def add_tags(tags)
-    tags.each { |tag| self.tags.build name: tag }
   end
 
   def as_json options={}
